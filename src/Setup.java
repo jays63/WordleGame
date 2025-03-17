82058aa3ce6acc5378c0dbe2557bc102f9309a0f
BT_1N3_13 <jays63@nycstudents.net>
2025-03-17 10:57:34 -0400

    Added the list of answers; To do: Allow guesses Display guesses Give feedback

--- src/Setup.java
+++ src/Setup.java
@@ -1,8 +1,32 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Setup {
-    private final String WORD;
+    private ArrayList<String> wordList;
+    private String word;
     private char[][] guesses;
 
     public Setup(){
         guesses=new char[6][5];
+    }
+
+    public void setWORD(){
+        wordList =new ArrayList<>();
+        try {
+            File words=new File("src\\ValidAnswers.txt");
+            Scanner sc=new Scanner(words);
+            while (sc.hasNext()){
+                wordList.add(sc.nextLine());
+            }
+            word =wordList.get((int)(Math.random()*wordList.size()));
+        } catch (IOException exception){
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    public String getWord(){
+        return word;
     }
 }
