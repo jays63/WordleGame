a5aa0ab08fdea3ec93f7391dc61ffa8a9d872f2e
jays63 <jays63@nycstudents.net>
2025-04-07 00:38:21 -0400

    Update Main.java
    Resolved a crash that occurs when trusting user input

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
         System.out.println("What mode would you like to play? \n1. Normal game\n2. Hard (Vowels will be expunged)");
-        int mode=s.nextInt();
+        int mode=0;
         WordleBase play;
+        try {
+            mode=s.nextInt();
+        } catch (InputMismatchException exception){
+            System.out.println("Oops, invalid input. Restart the program and try again, please.");
+            System.exit(0);
+        }
         switch (mode){
             case 1:
                 play=new NormalGame();
